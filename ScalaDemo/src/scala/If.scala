8ed47a99f0a4c2573baeb43eb9a73329307271dc
SearchWinter <QWER123qwer938$+++>
2020-08-26 11:22:04 +0800

    Initial commit

---
+++ ScalaDemo/src/scala/If.scala
@@ -0,0 +1,16 @@
+package scala
+
+/**
+ * @Description TODO
+ * @Date 2020/6/29  11:44
+ **/
+object If {
+  def main(args:Array[String]): Unit ={
+    var x = 10;
+    if(x<20){
+      println(x)
+    }else{
+      println(20)
+    }
+  }
+}
